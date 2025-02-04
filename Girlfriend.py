3a1098d480961252124595b540e0b309cc9dc38f
Luan de Oliveira <luannoliveira421@gmail.com>
2025-02-04 19:43:12 -0300

    Update Girlfriend.py

--- Girlfriend.py
+++ Girlfriend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import time as tm
+import time
 
 st.title('Oi, minha criança, fiz esse site para vc me responder uma :red[*SIMPLES*] perguntinha')
 st.header(':red[*VOCÊ*] quer namorar :blue[*COMIGO*]?')
@@ -8,17 +8,17 @@ if st.button(':blue[SIM]'):
 
 if st.button(':red[NÃO]'):
   st.write(':red[POQUIE NÃO?]')
-  tm.time(3.0)
+  time.time(3.0)
   st.write(':red[Achei desnecessário]')
-  tm.time(3.0)
+  time.time(3.0)
   st.write(':red[Vai ficar aqui para sempre]')
-  tm.time(3.0)
+  time.time(3.0)
   st.write(':red[Se apertar o botão que fala sim vc sai amoi]')
-  tm.time(3.0)
+  time.time(3.0)
   st.write(':red[Ainda não apertou?!]')
-  tm.time(3.0)
+  time.time(3.0)
   st.write(':red[APERTA LOGO]')
-  st.time(3.0)
+  time.time(3.0)
   st.write(':red[to esperando =(]')
   
   
