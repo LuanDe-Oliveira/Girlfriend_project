099115126ce1f79a2c509e60b35d0aef645d5f4a
Luan de Oliveira <luannoliveira421@gmail.com>
2025-02-04 19:13:33 -0300

    teste_Girlfriend.py

---
+++ Girlfriend.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import time as tm
+
+st.write('Oi, minha criança, fiz esse site para vc me responder uma :red[SIMPLES] perguntinha')
+st.header(':red[VOCÊ] quer namorar :blue[comigo]?')
+if st.button(':blue[SIM]'):
+  st.write('Certeza que foi de primeira 🙄')
+
+while if st.button(':red[NÃO]'):
+  st.write(':red[POQUIE NÃO?]')
+  tm.time(3.0)
+  st.write(':red[Achei desnecessário])
+  tm.time(3.0)
+  st.write(':red[Vai ficar aqui para sempre])
+  tm.time(3.0)
+  st.write(':red[Se apertar o botão que fala sim])
+  tm.time(3.0)
+  st.write(':red[Ainda não apertou?!])
+  tm.time(3.0)
+  st.write(':red[APERTA LOGO])
+  
+  
+  
+  
+  
+             
